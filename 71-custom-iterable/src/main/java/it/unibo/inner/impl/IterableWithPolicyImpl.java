eff8dd0093f22d9d191e686beaf89f82d48b862e
Matteo Todeschi - matteo.todeschi2@studio.unibo.it <matteo.todeschi2@studio.unibo.it>
2024-11-05 11:35:05 +0100

    Committed Exercise 71 -> Part 1

--- 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
@@ -7,10 +7,32 @@ import it.unibo.inner.api.Predicate;
 
 public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T>{
 
+    private T[] array;
+
+
+    public IterableWithPolicyImpl(T[] array) {
+        this.array = array;
+    }
+
+    private class PolicyIterator implements Iterator<T>{
+
+        private int position = 0;
+
+        @Override
+        public boolean hasNext() {
+            return position < array.length;
+        }
+
+        @Override
+        public T next() {
+            return array[position++];
+        }
+
+    }
+
     @Override
     public Iterator<T> iterator() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'iterator'");
+        return new PolicyIterator();
     }
 
     @Override
